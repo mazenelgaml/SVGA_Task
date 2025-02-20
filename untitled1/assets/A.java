c398d08a5ccbf75552cf151ed892e29c6d2a4ed9
Salma Elbasha <m.elgamal0551@gmail.com>
2025-02-20 18:38:50 +0200

    upload svga task

---
+++ untitled1/assets/A.java
@@ -0,0 +1,18 @@
+Performing hot restart...
+Syncing files to device SM A556E...
+Restarted application in 978ms.
+I/ViewRootImpl@be37324[MainActivity]( 5099): onDisplayChanged oldDisplayState=2 newDisplayState=2
+I/PlatformViewsController( 5099): Hosting view in view hierarchy for platform view: 0
+I/PlatformViewsController( 5099): PlatformView is using SurfaceProducer backend
+D/NativeCustomFrequencyManager( 5099): [NativeCFMS] BpCustomFrequencyManager::BpCustomFrequencyManager()
+W/vulkan  ( 5099): CreateSwapchainKHR(VkSwapchainCreateInfoKHR.imageColorSpace = 1000104013) failed: Unsupported color space, but assume as Success for ANGLE.
+E/FrameEvents( 5099): updateAcquireFence: Did not find frame.
+D/OpenGLRenderer( 5099): CFMS:: SetUp Pid : 5099    Tid : 5132
+D/NativeCustomFrequencyManager( 5099): [NativeCFMS] BpCustomFrequencyManager::BpCustomFrequencyManager()
+W/vulkan  ( 5099): CreateSwapchainKHR(VkSwapchainCreateInfoKHR.imageColorSpace = 1000104013) failed: Unsupported color space, but assume as Success for ANGLE.
+D/libEGL  ( 5099): dlopen (libGLESv2_samsung.so) success at 0xbd774eb8b3bf6a79
+E/FrameEvents( 5099): updateAcquireFence: Did not find frame.
+D/OpenGLRenderer( 5099): CFMS:: SetUp Pid : 5099    Tid : 5132
+E/FrameEvents( 5099): updateAcquireFence: Did not find frame.
+E/FrameEvents( 5099): updateAcquireFence: Did not find frame.
+I/ViewRootImpl@be37324[MainActivity]( 5099): onDisplayChanged oldDisplayState=2 newDisplayState=2
